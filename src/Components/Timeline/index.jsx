5dcd237e941ff9846d7a90518eacc7e64f2b828c
Lucas klafke <lucasnkz11@gmail.com>
2022-06-15 18:41:54 -0300

    after merge

---
+++ src/Components/Timeline/index.jsx
@@ -0,0 +1,59 @@
+import { useContext, useEffect, useState} from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import Post from "./post.jsx"
+import Header from '../PublicComponents/Header.js'
+export default function Timeline(){
+        const [allPosts, setAllPosts] = useState([]);
+
+        useEffect(() => {
+                const URL = 'http://localhost:4000/get/posts';
+                const promise = axios.get(URL);
+
+                promise.then((response) => {
+                        setAllPosts(response.data)
+                });
+                promise.catch(error => {
+                        console.log(error);
+                        alert("Deu algum erro...");
+                });
+        }, []);
+
+
+        return (
+                <>
+                        <Header />
+                        <PageContainer>
+                                <FeedContainer> 
+                                        <h2>Timeline</h2>
+                                        {allPosts.map(post => <Post  info={post} key={post.id}/>)}
+                                </FeedContainer>
+                        </PageContainer>
+                </>
+        )
+}
+
+const PageContainer = styled.div`
+        width: 100vw;
+        height: 100vh;
+        background-color: #333333;
+
+        display: flex;
+        justify-content:center;
+
+        
+`; 
+const FeedContainer = styled.div`
+        width: 615px;
+        margin-top: 100px;
+        h2{
+
+                font-family: 'Oswald';
+                font-style: normal;
+                font-weight: 700;
+                font-size: 43px;
+                line-height: 64px;
+                color: #FFFFFF;
+        }
+
+`
